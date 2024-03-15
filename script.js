56f2010148f7e2428d6869078ba06ec8aa37101a
Евгений Сергеев <sergeevem@rubezh.ru>
2024-03-15 10:57:16 +0400

    feat: add section ref, refact faq slider

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-const swiper = new Swiper('.swiper', {
+const faqSlider = new Swiper('#faq__slider', {
   // Optional parameters
   direction: 'horizontal',
   loop: false,
   slidesPerView: 1,
   spaceBetween: 24,
-
-  // Navigation arrows
+  pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+	},
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
+  navigation: {
+		enabled: false,
+	},
   breakpoints: {
 		524: {
 			slidesPerView: 2,
 		},
     768: {
       slidesPerView: 3,
+      pagination: {
+        enabled: false,
+      },
+      navigation: {
+        enabled: true,
+      },
     },
 		1024: {
 			slidesPerView: 4,
+      pagination: {
+        enabled: false,
+      },
+      navigation: {
+        enabled: true,
+      }
 		},
 	},
 });
